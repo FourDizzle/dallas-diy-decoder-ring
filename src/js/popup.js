dfbe25cb2c4097358e19bcc4b6aef7b21b1e7c7e
FourDizzle <nick@spinsci.com>
2017-01-07 17:00:45 -0600

    made basic layout for image creation

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,16 +1,21 @@
 import "../css/popup.css";
 import $ from 'jquery';
 
+var canvas  = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
 $(function () {
   $(':file').change(function () {
     if (this.files && this.files[0]) {
       var reader = new FileReader();
-      reader.onload = imageIsLoaded;
+      reader.onload = function(e) {
+        var img = new Image();
+        img.onload = function() {
+          context.drawImage(img, 0, 0);
+        };
+        img.src = e.target.result;
+      };
       reader.readAsDataURL(this.files[0]);
     }
   });
 });
-
-function imageIsLoaded(e) {
-  $('#my-img').attr('src', e.target.result);
-}
